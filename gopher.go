8a224efaee99776ff040a33af821c51dda024d49
Alireza <26859725+tralireza@users.noreply.github.com>
2017-06-02 15:34:00 +0100

    Simulation 1701m Average Waiting Time

--- gopher.go
+++ gopher.go
@@ -714,7 +714,7 @@ func maxNumEdgesToRemove(n int, edges [][]int) int {
 
 // 1701m Average Waiting Time
 func averageWaitingTime(customers [][]int) float64 {
-	w, clock := float64(0), 0
+	waiting, clock := 0, 0
 
 	for i := range customers {
 		arrive, time := customers[i][0], customers[i][1]
@@ -722,10 +722,10 @@ func averageWaitingTime(customers [][]int) float64 {
 			clock = arrive
 		}
 		clock += time
-		w += float64(clock - arrive)
+		waiting += clock - arrive
 	}
 
-	return w / float64(len(customers))
+	return float64(waiting) / float64(len(customers))
 }
 
 // 1823m Find the Winner of the Circular Game
