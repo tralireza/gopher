af1c4b3c55c9f36afc8c30bcd31905faa1ca49ff
Alireza <26859725+tralireza@users.noreply.github.com>
2017-09-16 00:34:00 +0100

    Binary Tree 1110m Delete Nodes And Return Forest -> iterative

--- bt_test.go
+++ bt_test.go
@@ -25,15 +25,62 @@ func Test1110(t *testing.T) {
 		}
 	}
 
+	Iterative := func(root *TreeNode, to_delete []int) []*TreeNode {
+		D := make([]bool, 1000+1)
+		for _, n := range to_delete {
+			D[n] = true
+		}
+
+		F := []*TreeNode{}
+
+		Q := []*TreeNode{root}
+		var n *TreeNode
+		for len(Q) > 0 {
+			n, Q = Q[0], Q[1:]
+
+			if n.Left != nil {
+				Q = append(Q, n.Left)
+				if D[n.Left.Val] {
+					n.Left = nil
+				}
+			}
+			if n.Right != nil {
+				Q = append(Q, n.Right)
+				if D[n.Right.Val] {
+					n.Right = nil
+				}
+			}
+
+			if D[n.Val] {
+				if n.Left != nil {
+					F = append(F, n.Left)
+				}
+				if n.Right != nil {
+					F = append(F, n.Right)
+				}
+			}
+		}
+
+		if !D[root.Val] {
+			F = append(F, root)
+		}
+
+		return F
+	}
+
 	type T = TreeNode
 
-	tree := &T{1, &T{2, &T{Val: 4}, &T{Val: 5}}, &T{3, &T{Val: 6}, &T{Val: 7}}}
-	Draw(tree)
-	log.Print(" -> ")
-
-	for _, tree := range delNodes(tree, []int{3, 5}) {
-		fmt.Println(":: ")
+	for _, f := range []func(*TreeNode, []int) []*TreeNode{delNodes, Iterative} {
+		tree := &T{1, &T{2, &T{Val: 4}, &T{Val: 5}}, &T{3, &T{Val: 6}, &T{Val: 7}}}
 		Draw(tree)
+		log.Print(" -> ")
+
+		for _, tree := range f(tree, []int{3, 5}) {
+			fmt.Println(":: ")
+			Draw(tree)
+		}
+
+		log.Print("--")
 	}
 }
 
