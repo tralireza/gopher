ff59b555b0aa8d7731b846abf91ec9035a693b6f
Alireza <26859725+tralireza@users.noreply.github.com>
2017-09-26 11:42:00 +0100

    Sorting 2418 Sort the People

--- sort_test.go
+++ sort_test.go
@@ -2,11 +2,31 @@ package gopher
 
 import (
 	"log"
+	"slices"
 	"testing"
 )
 
 // 2418 Sort the People
 func Test2418(t *testing.T) {
-	log.Print("[Mary Emma John] ?= ", sortPeople([]string{"Mary", "John", "Emma"}, []int{180, 165, 170}))
-	log.Print("[Bob Alice Bob] ?= ", sortPeople([]string{"Alice", "Bob", "Bob"}, []int{155, 185, 150}))
+	WithIndex := func(names []string, heights []int) []string {
+		type P struct{ i, h int }
+
+		D := []P{}
+		for i := range names {
+			D = append(D, P{i: i, h: heights[i]})
+		}
+		slices.SortFunc(D, func(x, y P) int { return y.h - x.h })
+
+		R := []string{}
+		for i := range D {
+			R = append(R, names[D[i].i])
+		}
+		return R
+	}
+
+	for _, f := range []func([]string, []int) []string{sortPeople, WithIndex} {
+		log.Print("[Mary Emma John] ?= ", f([]string{"Mary", "John", "Emma"}, []int{180, 165, 170}))
+		log.Print("[Bob Alice Bob] ?= ", f([]string{"Alice", "Bob", "Bob"}, []int{155, 185, 150}))
+		log.Print("--")
+	}
 }
