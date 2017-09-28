0ffa467c3cee898baba1df74eed1ca41c02d33b4
Alireza <26859725+tralireza@users.noreply.github.com>
2017-09-28 13:35:00 +0100

    Sorting 2418 Sort the People -> QuickSort

--- sort_test.go
+++ sort_test.go
@@ -8,6 +8,31 @@ import (
 
 // 2418 Sort the People
 func Test2418(t *testing.T) {
+	QuickSort := func(names []string, heights []int) []string {
+		var qSort func(s, e int)
+		qSort = func(s, e int) {
+			if s >= e {
+				return
+			}
+
+			// Pivot Value: element at first index :: PivotVal <- Arrry[start]
+			v, p := heights[s], s
+			for i := s + 1; i <= e; i++ {
+				if heights[i] > v {
+					heights[i], heights[p] = heights[p], heights[i]
+					names[i], names[p] = names[p], names[i]
+					p++
+				}
+			}
+
+			qSort(s, p-1)
+			qSort(p+1, e)
+		}
+		qSort(0, len(heights)-1)
+
+		return names
+	}
+
 	WithIndex := func(names []string, heights []int) []string {
 		type P struct{ i, h int }
 
@@ -24,9 +49,10 @@ func Test2418(t *testing.T) {
 		return R
 	}
 
-	for _, f := range []func([]string, []int) []string{sortPeople, WithIndex} {
+	for _, f := range []func([]string, []int) []string{sortPeople, WithIndex, QuickSort} {
 		log.Print("[Mary Emma John] ?= ", f([]string{"Mary", "John", "Emma"}, []int{180, 165, 170}))
 		log.Print("[Bob Alice Bob] ?= ", f([]string{"Alice", "Bob", "Bob"}, []int{155, 185, 150}))
+		log.Print("[A B C D E F G] ?= ", f([]string{"A", "F", "G", "B", "D", "C", "E"}, []int{7, 2, 1, 6, 4, 5, 3}))
 		log.Print("--")
 	}
 }
