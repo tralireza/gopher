4c57a35ed4b6e7cff3952aaab955a40d0c059ccc
Alireza <26859725+tralireza@users.noreply.github.com>
2017-09-28 13:52:00 +0100

    Sorting 2418 Sort the People -> MergeSort :: worst-case: O(NlogN)

--- sort_test.go
+++ sort_test.go
@@ -9,6 +9,7 @@ import (
 // 2418 Sort the People
 func Test2418(t *testing.T) {
 	QuickSort := func(names []string, heights []int) []string {
+		// worse-case: O(N^2)
 		var qSort func(s, e int)
 		qSort = func(s, e int) {
 			if s >= e {
@@ -33,6 +34,38 @@ func Test2418(t *testing.T) {
 		return names
 	}
 
+	MergeSort := func(names []string, heights []int) []string {
+		// worse-case: O(NlogN)
+		var mSort func(s, e int)
+		mSort = func(s, e int) {
+			if s >= e {
+				return
+			}
+
+			m := s + (e-s)>>1
+			mSort(s, m)
+			mSort(m+1, e)
+
+			// Merge
+			Ht, Nt := make([]int, e-s+1), make([]string, e-s+1) // Heights/Names merge temporary storage
+			l, r := s, m+1
+			for i := 0; i <= e-s; i++ {
+				if l <= m && (r > e || heights[l] >= heights[r]) {
+					Ht[i], Nt[i] = heights[l], names[l]
+					l++
+				} else {
+					Ht[i], Nt[i] = heights[r], names[r]
+					r++
+				}
+			}
+			copy(heights[s:], Ht)
+			copy(names[s:], Nt)
+		}
+		mSort(0, len(heights)-1)
+
+		return names
+	}
+
 	WithIndex := func(names []string, heights []int) []string {
 		type P struct{ i, h int }
 
@@ -49,7 +82,7 @@ func Test2418(t *testing.T) {
 		return R
 	}
 
-	for _, f := range []func([]string, []int) []string{sortPeople, WithIndex, QuickSort} {
+	for _, f := range []func([]string, []int) []string{sortPeople, WithIndex, QuickSort, MergeSort} {
 		log.Print("[Mary Emma John] ?= ", f([]string{"Mary", "John", "Emma"}, []int{180, 165, 170}))
 		log.Print("[Bob Alice Bob] ?= ", f([]string{"Alice", "Bob", "Bob"}, []int{155, 185, 150}))
 		log.Print("[A B C D E F G] ?= ", f([]string{"A", "F", "G", "B", "D", "C", "E"}, []int{7, 2, 1, 6, 4, 5, 3}))
