0cebecaf3ddf93658ff0c22b219ab0dd2f93b481
Alireza <26859725+tralireza@users.noreply.github.com>
2017-10-01 18:03:00 +0100

    Graph 2392h Build a Matrix With Conditions -> Topological Sort with DFS on all 'white' nodes

--- graph_test.go
+++ graph_test.go
@@ -2,12 +2,54 @@ package gopher
 
 import (
 	"log"
+	"slices"
 	"testing"
 )
 
 // 2392h Build a Matrix With Conditions
 func Test2392(t *testing.T) {
 	// 1 <= Node(labels) <= k
+
+	TopoOrder := func(k int, edges [][]int) []int {
+		G := make([][]int, k+1)
+		for _, e := range edges {
+			G[e[0]] = append(G[e[0]], e[1])
+		}
+
+		Color := make([]int, k+1) // Visited:Color of a Node: 0: white, 1: gray, 2: black
+		Color[0] = -1             // ignore Node at label 0
+
+		tSort := []int{}
+		var DFS func(int) bool
+		DFS = func(v int) bool {
+			Color[v] = 1 // Gray as in Visiting...
+			for _, u := range G[v] {
+				switch Color[u] {
+				case 0:
+					if DFS(u) {
+						return true
+					}
+				case 1:
+					return true // cycle detected
+				}
+			}
+			Color[v] = 2 // Black as in Done
+			tSort = append(tSort, v)
+			return false
+		}
+
+		for n := range Color {
+			if Color[n] == 0 {
+				if DFS(n) {
+					return []int{}
+				}
+			}
+		}
+
+		slices.Reverse(tSort)
+		return tSort
+	}
+	log.Print("Topological Order (DFS): ", TopoOrder(3, [][]int{{1, 2}, {3, 2}}))
 
 	log.Print("[[3 0 0] [0 0 1] [0 2 0]] ?= ", buildMatrix(3, [][]int{{1, 2}, {3, 2}}, [][]int{{2, 1}, {3, 2}}))
 	log.Print("[] ?= ", buildMatrix(3, [][]int{{1, 2}, {2, 3}, {3, 1}, {2, 3}}, [][]int{{2, 1}}))
