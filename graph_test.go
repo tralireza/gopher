aed519fc110be75045942e3129d4b7b7d9b8ffe2
Alireza <26859725+tralireza@users.noreply.github.com>
2017-10-08 05:32:00 +0100

    Graph 1334m Find the City With the Smallest Number of Neighbors at a Threshold Distance -> run Bellman-Ford for every node as source

--- graph_test.go
+++ graph_test.go
@@ -2,6 +2,7 @@ package gopher
 
 import (
 	"log"
+	"math"
 	"slices"
 	"testing"
 )
@@ -10,8 +11,44 @@ import (
 func Test1334(t *testing.T) {
 	// 1 <= Weight_i <= 10^4
 
-	log.Print("3 ?= ", findTheCity(4, [][]int{{0, 1, 3}, {1, 2, 1}, {1, 3, 4}, {2, 3, 1}}, 4))
-	log.Print("0 ?= ", findTheCity(5, [][]int{{0, 1, 2}, {0, 4, 8}, {1, 2, 3}, {1, 4, 2}, {2, 3, 1}, {3, 4, 1}}, 2))
+	BellmanFord := func(n int, edges [][]int, distanceThreshold int) int {
+		aSP := make([][]int, n)
+		for r := range aSP {
+			aSP[r] = make([]int, n)
+			for c := range aSP[r] {
+				aSP[r][c] = math.MaxInt
+			}
+		}
+
+		BellmanFord := func(s int, SP []int) {
+			SP[s] = 0
+			for range n - 1 {
+				for _, e := range edges {
+					v, u, w := e[0], e[1], e[2]
+					if SP[v] != math.MaxInt && SP[v]+w < SP[u] {
+						SP[u] = SP[v] + w
+					}
+					if SP[u] != math.MaxInt && SP[u]+w < SP[v] {
+						SP[v] = SP[u] + w
+					}
+				}
+			}
+		}
+
+		for s := range n {
+			BellmanFord(s, aSP[s])
+		}
+
+		log.Print("Bellman-Ford: ", aSP)
+
+		return 0
+	}
+
+	for _, f := range []func(int, [][]int, int) int{findTheCity, BellmanFord} {
+		log.Print("3 ?= ", f(4, [][]int{{0, 1, 3}, {1, 2, 1}, {1, 3, 4}, {2, 3, 1}}, 4))
+		log.Print("0 ?= ", f(5, [][]int{{0, 1, 2}, {0, 4, 8}, {1, 2, 3}, {1, 4, 2}, {2, 3, 1}, {3, 4, 1}}, 2))
+		log.Print("--")
+	}
 }
 
 // 2392h Build a Matrix With Conditions
