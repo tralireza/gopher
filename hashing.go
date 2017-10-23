a839d3fc30ef628f82c2a4ac7bb12d2d9e6c491c
Alireza <26859725+tralireza@users.noreply.github.com>
2017-10-23 14:26:00 +0100

    Hashing 75h Minimum Window Substring

--- hashing.go
+++ hashing.go
@@ -1,5 +1,47 @@
 package gopher
 
+// 76h Minimum Window Substring
+func minWindow(s string, t string) string {
+	idx := func(l byte) int {
+		switch {
+		case 'a' <= l && l <= 'z':
+			return int(l - 'a')
+		default:
+			return int(l-'A') + 26
+		}
+	}
+
+	hS, hT := [52]int{}, [52]int{}
+
+	isGood := func() bool {
+		for i := 0; i < 52; i++ {
+			if hS[i] < hT[i] {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := 0; i < len(t); i++ {
+		hT[idx(t[i])]++
+	}
+
+	l, minStr := 0, ""
+	for r := 0; r < len(s); r++ {
+		hS[idx(s[r])]++
+
+		for isGood() {
+			if minStr == "" || r-l < len(minStr) {
+				minStr = s[l : r+1]
+			}
+
+			hS[idx(s[l])]--
+			l++
+		}
+	}
+	return minStr
+}
+
 // 438m Find All Anagrams in a String
 func findAnagrams(s string, p string) []int {
 	fP, fS := [26]int{}, [26]int{}
--- hashing_test.go
+++ hashing_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+// 76h Minimum Window Substring
+func Test76(t *testing.T) {
+	log.Print("BANC ?= ", minWindow("ADOBECODEBANC", "ABC"))
+	log.Print("a ?= ", minWindow("aa", "a"))
+	log.Print(" ?= ", minWindow("a", "aa"))
+}
+
 // 438m Find All Anagrams in a String
 func Test438(t *testing.T) {
 	log.Print("[0 6] ?= ", findAnagrams("cbaebabacd", "abc"))
