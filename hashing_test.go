8c17e48a42705dab9211d75c1620500aacd91947
Alireza <26859725+tralireza@users.noreply.github.com>
2017-11-05 08:37:00 +0000

    Hashing 1450 Make Two Arrays Equal by Reversing Subarrays -> Optimized: one hashtable only

--- hashing_test.go
+++ hashing_test.go
@@ -22,7 +22,25 @@ func Test438(t *testing.T) {
 func Test1460(t *testing.T) {
 	// 1 <= Ai <= 10000
 
-	log.Print("true ?= ", canBeEqual([]int{1, 2, 3, 4}, []int{2, 4, 1, 3}))
-	log.Print("true ?= ", canBeEqual([]int{7}, []int{7}))
-	log.Print("false ?= ", canBeEqual([]int{3, 7, 9}, []int{3, 7, 11}))
+	Optimized := func(target, arr []int) bool {
+		hA := make([]int, 10_000+1)
+		for _, n := range arr {
+			hA[n]++
+		}
+
+		for _, n := range target {
+			if hA[n] == 0 {
+				return false
+			}
+			hA[n]--
+		}
+		return true
+	}
+
+	for _, f := range []func([]int, []int) bool{canBeEqual, Optimized} {
+		log.Print("true ?= ", f([]int{1, 2, 3, 4}, []int{2, 4, 1, 3}))
+		log.Print("true ?= ", f([]int{7}, []int{7}))
+		log.Print("false ?= ", f([]int{3, 7, 9}, []int{3, 7, 11}))
+		log.Print("--")
+	}
 }
