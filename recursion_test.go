d15a7e895e9d464c51a94646aff16e3ac6a89998
Alireza <26859725+tralireza@users.noreply.github.com>
2017-12-09 21:59:00 +0000

    Recursion 650m 2 Keys Keyboard -> BottomUp

--- recursion_test.go
+++ recursion_test.go
@@ -13,8 +13,29 @@ func Test40(t *testing.T) {
 
 // 650m 2 Keys Keyboard
 func Test650(t *testing.T) {
-	log.Print("3 ?= ", minSteps(3))
-	log.Print("0 ?= ", minSteps(1))
-	log.Print("8 ?= ", minSteps(15))
-	log.Print("21 ?= ", minSteps(1000))
+	// 1 <= n <= 1000
+
+	BottomUp := func(n int) int {
+		// f(i) = f(j) + i/j | i mod j == 0
+
+		D := make([]int, 1001)
+		for i := 2; i <= n; i++ {
+			D[i] = 1000 // init -> Max
+
+			for j := 1; j <= n/2; j++ {
+				if i%j == 0 {
+					D[i] = min(D[i], D[j]+i/j)
+				}
+			}
+		}
+		return D[n]
+	}
+
+	for _, f := range []func(int) int{minSteps, BottomUp} {
+		log.Print("3 ?= ", f(3))
+		log.Print("0 ?= ", f(1))
+		log.Print("8 ?= ", f(15))
+		log.Print("21 ?= ", f(1000))
+		log.Print("--")
+	}
 }
