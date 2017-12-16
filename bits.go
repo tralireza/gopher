721acf367c347a362aa99043df2e9fc881f0c2be
Alireza <26859725+tralireza@users.noreply.github.com>
2017-12-16 09:50:00 +0000

    Bits 476 Number Complement

--- bits.go
+++ bits.go
@@ -2,14 +2,9 @@ package gopher
 
 // 476 Number Complement
 func findComplement(num int) int {
-	if num == 0 {
-		return 1
-	}
-
 	bits := 0
 	for x := num; x > 0; x >>= 1 {
 		bits++
 	}
-
 	return (1<<bits - 1) ^ num
 }
