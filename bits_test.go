3385b9289e300b6f5aeb86a904b97a9e1e3c5279
Alireza <26859725+tralireza@users.noreply.github.com>
2017-12-16 09:55:00 +0000

    Bits 476 Number Complement

--- bits_test.go
+++ bits_test.go
@@ -2,12 +2,17 @@ package gopher
 
 import (
 	"log"
+	"math/bits"
 	"testing"
 )
 
 // 476 Number Complement
 func Test476(t *testing.T) {
 	// 1 <= n <= 2^31
+
+	log.Print("C(7) -> ", (1<<bits.Len(7)-1)^7)
+	log.Print("C(5) -> ", (1<<bits.Len(5)-1)^5)
+	log.Print("--")
 
 	log.Print("2 ?= ", findComplement(5))
 	log.Print("0 ?= ", findComplement(1))
