90dbbc7ba0b84bfda5c6da024afd79594894b567
Alireza <26859725+tralireza@users.noreply.github.com>
2018-02-10 16:26:00 +0000

    Binary Tree 103m Binary Tree Zigzag Level Order Traversal

--- bintree_test.go
+++ bintree_test.go
@@ -12,4 +12,6 @@ func Test103(t *testing.T) {
 	log.Print("[[3] [20 9] [15 7]] ?= ", zigzagLevelOrder(&T{3, &T{Val: 9}, &T{20, &T{Val: 15}, &T{Val: 7}}}))
 	log.Print("[[1]] ?= ", zigzagLevelOrder(&T{Val: 1}))
 	log.Print("[] ?= ", zigzagLevelOrder(nil))
+
+	log.Print("[[1] [3 2] [4 5]] ?= ", zigzagLevelOrder(&T{1, &T{2, &T{Val: 4}, &T{Val: 5}}, &T{Val: 3}}))
 }
