73982d377683db061555d4cd204442e0c6403d0f
Alireza <26859725+tralireza@users.noreply.github.com>
2018-05-01 11:22:00 +0100

    Math 319m Bulb Switcher

--- math.go
+++ math.go
@@ -2,11 +2,7 @@ package gopher
 
 // 319m Bulb Switcher
 func bulbSwitch(n int) int {
-	if n <= 1 {
-		return n
-	}
-
-	q := 2 // Square Root
+	q := 0 // Square Root
 	for q*q <= n {
 		q++
 	}
--- math_test.go
+++ math_test.go
@@ -11,5 +11,6 @@ func Test319(t *testing.T) {
 	log.Print("0 ?= ", bulbSwitch(0))
 	log.Print("1 ?= ", bulbSwitch(1))
 
+	log.Print("3 ?= ", bulbSwitch(9))
 	log.Print("5 ?= ", bulbSwitch(27))
 }
