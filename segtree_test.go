101a4f9bab8063bebe4e77e3590757f9d14290f6
Alireza <26859725+tralireza@users.noreply.github.com>
2018-05-27 03:14:00 +0100

    Segment Tree 307m Range Sum Query - Mutable -> Fenwick Tree :: Binary Indexed Tree (BIT)

--- segtree_test.go
+++ segtree_test.go
@@ -7,6 +7,37 @@ import (
 )
 
 // 307m Range Sum Query - Mutable
+type FenwickSum307 struct {
+	Tree []int
+}
+
+func NewFenwickSum307(arr []int) FenwickSum307 {
+	o := FenwickSum307{
+		make([]int, len(arr)),
+	}
+
+	for i := 0; i < len(o.Tree); i++ {
+		o.Update(i, arr[i])
+	}
+	return o
+}
+
+func (o *FenwickSum307) Update(i, delta int) {
+	for ; i < len(o.Tree); i |= i + 1 {
+		o.Tree[i] += delta
+	}
+}
+
+func (o *FenwickSum307) Sum(i int) int {
+	v := 0
+	for i >= 0 {
+		v += o.Tree[i]
+		i &= i + 1
+		i--
+	}
+	return v
+}
+
 func Test307(t *testing.T) {
 	Draw := func(n *SNode307) {
 		Q := []*SNode307{n}
@@ -28,6 +59,12 @@ func Test307(t *testing.T) {
 		}
 	}
 
+	fws := NewFenwickSum307([]int{1, 3, 5})
+	log.Print(" -> FenwickTree Sum (0..2) :: ", fws.Sum(2))
+	log.Print(" -> FenwickTree Sum (1..2) :: ", fws.Sum(2)-fws.Sum(0))
+	fws.Update(1, -1) // N[1] :: 3 --[delta: -1]-> 2
+	log.Print(" -> FenwickTree Sum (1..2) :: ", fws.Sum(2)-fws.Sum(0))
+
 	o := Constructor307([]int{1, 3, 5})
 	Draw(o.Tree)
 
@@ -39,6 +76,7 @@ func Test307(t *testing.T) {
 
 // 731m My Calendar II
 func Test731(t *testing.T) {
+	// 0 <= Event(start, end) <= 10^9
 	events := [][]int{{10, 20}, {50, 60}, {10, 40}, {5, 15}, {5, 10}, {25, 55}}
 	results := []bool{true, true, true, false, true, true}
 
