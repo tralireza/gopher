92c2ed57a17a8070c19b67ef360e7788a9c40144
Alireza <26859725+tralireza@users.noreply.github.com>
2018-06-02 03:01:00 +0100

    Math 3312h Sorted GCD Pair Queries

--- math.go
+++ math.go
@@ -1,5 +1,10 @@
 package gopher
 
+import (
+	"log"
+	"slices"
+)
+
 // 319m Bulb Switcher
 func bulbSwitch(n int) int {
 	q := 0 // Square Root
@@ -7,4 +12,53 @@ func bulbSwitch(n int) int {
 		q++
 	}
 	return q - 1
+}
+
+// 3312h Sorted GCD Pair Queries
+func gcdValues(nums []int, queries []int64) []int {
+	xVal := slices.Max(nums)
+	freq := make([]int, xVal+1)
+	for _, n := range nums {
+		freq[n]++
+	}
+
+	log.Print(" -> frequency :: ", freq)
+
+	GCD := make([]int, xVal+1) // count of Pairs: {Ni, Nj} with gcd(Ni, Nj) = GCD[g]
+	for g := xVal; g >= 1; g-- {
+		count := 0
+		for m := g; m <= xVal; m += g { // multiples of g
+			count += freq[m]
+		}
+		GCD[g] = count * (count - 1) / 2 // nC2 ~ n!/2!.(n-2)!
+
+		for m := 2 * g; m <= xVal; m += g { // remove double-counted
+			GCD[g] -= GCD[m]
+		}
+	}
+
+	log.Print(" -> GCD[g] :: ", GCD)
+
+	pSum := make([]int64, xVal+1)
+	pSum[0] = int64(GCD[0])
+	for g := 1; g <= xVal; g++ {
+		pSum[g] = pSum[g-1] + int64(GCD[g])
+	}
+
+	log.Print(" -> Sigma GCD[g] :: ", pSum)
+
+	R := []int{}
+	for _, q := range queries {
+		l, r := 0, xVal+1
+		for l < r {
+			m := l + (r-l)>>1
+			if pSum[m] > q {
+				r = m
+			} else {
+				l = m + 1
+			}
+		}
+		R = append(R, r)
+	}
+	return R
 }
--- math_test.go
+++ math_test.go
@@ -14,3 +14,12 @@ func Test319(t *testing.T) {
 	log.Print("3 ?= ", bulbSwitch(9))
 	log.Print("5 ?= ", bulbSwitch(27))
 }
+
+// 3312h Sorted GCD Pair Queries
+func Test3312(t *testing.T) {
+	// 1 <= N_i <= 5*10^4, N.length <= 10^5
+
+	log.Print("[1 2 2] ?= ", gcdValues([]int{2, 3, 4}, []int64{0, 2, 2}))
+	log.Print("[4 2 1 1] ?= ", gcdValues([]int{4, 4, 2, 1}, []int64{5, 3, 1, 0}))
+	log.Print("[2 2] ?= ", gcdValues([]int{2, 2}, []int64{0, 0}))
+}
