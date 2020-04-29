d5a9b1e76dde614f578f9bc1b8cde00f63a79c03
Alireza <26859725+tralireza@users.noreply.github.com>
2020-04-29 20:04:45 +0000

    Simulation 1389 Create Target Array in the Given Order
    === RUN   Test1389
    * [0 1 2 3 4] [0 1 2 2 1]
    :: [0 4 1 3 2]
    * [1 2 3 4 0] [0 1 2 3 0]
    :: [0 1 2 3 4]
    * [1] [0]
    :: [1]
    --- PASS: Test1389 (0.00s)
    PASS
    ok  	gopher	0.002s

--- simulation_test.go
+++ simulation_test.go
@@ -122,6 +122,8 @@ func Test1389(t *testing.T) {
 		rst, nums, index []int
 	}{
 		{[]int{0, 4, 1, 3, 2}, []int{0, 1, 2, 3, 4}, []int{0, 1, 2, 2, 1}},
+		{[]int{0, 1, 2, 3, 4}, []int{1, 2, 3, 4, 0}, []int{0, 1, 2, 3, 0}},
+		{[]int{1}, []int{1}, []int{0}},
 	} {
 		log.Print("* ", c.nums, c.index)
 		if !reflect.DeepEqual(c.rst, createTargetArray(c.nums, c.index)) {
